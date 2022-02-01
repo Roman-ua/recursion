bdec03f0e7c8c31767aec6414f73a233e8d17631
roman148 <roman@u-coders.team>
2022-02-01 10:47:49 +0200

    add new recursion

---
+++ from_start_to_N/from_start_to_n.js
@@ -0,0 +1,15 @@
+const second_output_data = document.getElementById('second_output')
+const second_preparation_button = document.getElementById('second_preparation_data')
+const input = document.getElementById('second_screen_input')
+let initial = [0]
+const calculateData = (input_value) => {
+  if(initial[initial.length -1] < input_value) {
+    initial.push(initial[initial.length - 1] + 1)
+    calculateData(input_value)
+  }else {
+    second_output_data.innerText = `${initial}`
+  }
+}
+console.log(initial)
+second_preparation_button.addEventListener('click', ()=>calculateData(input.value))
+
